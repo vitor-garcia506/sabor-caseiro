305ff1359c0a3d5cedf7135de4a66c98c7c25524
Vitor Souza <garcia.souza450@outlook.com>
2024-06-29 05:37:42 -0300

    Pasta backend deletada

--- backend/templates/cabecalho.php
+++
@@ -1,4 +0,0 @@
-<header class="cabecalho">
-    <img src="img/logotipo.svg" alt="Logotipo do Sabor Caseiro" class="logotipo">
-    <h1 class="tituloPrincipal">Sabor Caseiro</h1>
-</header>
--- backend/templates/rodape.php
+++
@@ -1,5 +0,0 @@
-<footer class="rodape">
-    <p class="direitosAutorais">
-        Desenvolvido por <a href="https://github.com/vitorGarcia506" target="_blank" class="linkDireitosAutorais">Vitor Souza</a> &copy; <?php echo date("Y"); ?>
-    </p>
-</footer>
